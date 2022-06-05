ad5cb93c9f3751d11d20919e716e7bb9c0882ed8
“Xiaoxi <xl59hozy@studserv.uni-leipzig.de>
2022-06-05 19:42:16 +0200

    test gradoop impl

--- gradoop_tempo/src/main/java/Bike_test.java
+++ gradoop_tempo/src/main/java/Bike_test.java
@@ -1,8 +1,8 @@
-import Data.TemporalCitiBikeGraph;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.gradoop.common.model.impl.id.GradoopId;
+import org.gradoop.examples.common.TemporalCitiBikeGraph;
 import org.gradoop.flink.model.impl.operators.sampling.functions.VertexDegree;
 import org.gradoop.flink.util.GradoopFlinkConfig;
 import org.gradoop.temporal.model.api.TimeDimension;
@@ -21,17 +21,28 @@ public class Bike_test {
         // get data
         TemporalGraph graph = TemporalCitiBikeGraph.getTemporalGraph(GradoopFlinkConfig.createConfig(env));
 
+        // take the needed subgraph
+        TemporalGraph myGraph = graph.subgraph(
+                v -> v.getLabel().equals("Station"),
+                e -> true
+        ).verify();
+
+        myGraph.print();
+
         // set the time dimension
         TimeDimension timeDimension = TimeDimension.VALID_TIME;
 
         // set the DegreeType
         VertexDegree degreeType = VertexDegree.BOTH;
 
-        // get the result via operator
+        // set the parameter of operator
         TemporalVertexDegree operator = new TemporalVertexDegree(degreeType, timeDimension);
+        operator.setIncludeVertexTime(false);
+
+        // get the result via operator
         DataSet<Tuple4<GradoopId, Long, Long, Integer>> resultDataSet = graph.callForValue(operator);
 
-        // print the result on the console
+        // print the result
         resultDataSet.print();
 
         // calculate the runtime
